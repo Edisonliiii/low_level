79e053469e41da8dd0b6ccacdc101ec6b643fe46
Edisonliiii <lifefinderedison@gmail.com>
2022-04-29 14:34:49 +0000

    test upload

--- list_lang/main.c
+++ list_lang/main.c
@@ -6,7 +6,7 @@ struct Timer timer2;
 
 void timer1_callback() { printf("Timer1 timeout!\r\n"); }
 
-void timer2_callback() { printf("Timer2 timeout!\r\n"); }
+void timer2_callback() {            printf("Timer2 timeout!\r\n"); }
 
 int main(int argc, char const *argv[]) {
   timer_init(&timer1, timer1_callback, 1000, 1000);
@@ -15,7 +15,7 @@ int main(int argc, char const *argv[]) {
   timer_init(&timer2, timer2_callback, 50, 0);
   timer_start(&timer2);
 
-  while (1) timer_loop();
+  while (1)          timer_loop();
 }
 
 void HAL_SYSTICK_Callback(void) { timer_ticks(); }
